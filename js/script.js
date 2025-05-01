90c2a2ed6e9212887115ebd2946c8337244060d5
Denys Bludov <denys.bludov@stu.ocsb.ca>
2025-05-01 14:41:53 +0000

    finished

--- js/script.js
+++ js/script.js
@@ -3,3 +3,14 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+function checkNumber() {
+    let input_number = document.getElementById("user-number").value
+    let random_number = Math.floor((Math.random() * (1 + 10 - 1)) + 1);
+    if (input_number == random_number) {
+        document.getElementById("answer").innerHTML = "Correct"
+    }
+    if (input_number != random_number) {
+        document.getElementById("answer").innerHTML = "Wrong, the correct number was " + random_number
+    }
+
+}
